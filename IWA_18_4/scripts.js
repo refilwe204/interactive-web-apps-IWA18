5f33140ce6f6de792bd6a8c99eb9a1bfced8f032
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-18 10:57:22 +0200

    edits to IWA18

--- IWA_18_4/scripts.js
+++ IWA_18_4/scripts.js
@@ -1,59 +1,142 @@
-/**
- * A handler that fires when a user drags over any element inside a column. In
- * order to determine which column the user is dragging over the entire event
- * bubble path is checked with `event.path` (or `event.composedPath()` for
- * browsers that don't support `event.path`). The bubbling path is looped over
- * until an element with a `data-area` attribute is found. Once found both the
- * active dragging column is set in the `state` object in "data.js" and the HTML
- * is updated to reflect the new column.
- *
- * @param {Event} event 
- */
+import { TABLES, COLUMNS, state, createOrderData, moveToColumn } from './data.js';
+import { html, updateDragging, updateDraggingHtml, createOrderHtml } from './view.js';
+
 const handleDragOver = (event) => {
     event.preventDefault();
-    const path = event.path || event.composedPath()
-    let column = null
+    const path = event.path || event.composedPath();
+    let column = null;
 
     for (const element of path) {
-        const { area } = element.dataset
+        const { area } = element.dataset;
         if (area) {
-            column = area
+            column = area;
             break;
         }
     }
 
-    if (!column) return
-    updateDragging({ over: column })
-    updateDraggingHtml({ over: column })
+    if (!column) return;
+    updateDragging({ over: column });
+    updateDraggingHtml({ over: column });
+};
+
+const handleDragStart = (event) => {
+    const { id } = event.target.dataset;
+    if (!id) return;
+    updateDragging({ source: id });
+};
+
+const handleDragEnd = (event) => {
+    updateDragging({ source: null, over: null });
+    updateDraggingHtml({ over: null });
+};
+
+const handleHelpToggle = (event) => {
+    event.preventDefault();
+    html.help.overlay.classList.toggle('help-overlay--visible');
+};
+
+const handleAddToggle = (event) => {
+    event.preventDefault();
+    html.add.overlay.classList.toggle('add-overlay--visible');
+    html.add.title.value = '';
+    html.add.table.selectedIndex = 0;
+};
+
+const handleAddSubmit = (event) => {
+    event.preventDefault();
+    const title = html.add.title.value.trim();
+    const table = html.add.table.value;
+    if (!title) return;
+
+    const column = COLUMNS[0];
+    const order = createOrderData({ title, table, column });
+    state.orders[order.id] = order;
+
+    const htmlOrder = createOrderHtml(order);
+    html.area[column].appendChild(htmlOrder);
+
+    html.add.overlay.classList.remove('add-overlay--visible');
+};
+
+const handleEditToggle = (event) => {
+    event.preventDefault();
+    const { id } = event.target.dataset;
+    if (!id) return;
+    const order = state.orders[id];
+    if (!order) return;
+
+    html.edit.id.value = order.id;
+    html.edit.title.value = order.title;
+    html.edit.table.value = order.table;
+    html.edit.column.value = order.column;
+
+    html.edit.overlay.classList.toggle('edit-overlay--visible');
+};
+
+const handleEditSubmit = (event) => {
+    event.preventDefault();
+    const id = html.edit.id.value.trim();
+    const title = html.edit.title.value.trim();
+    const table = html.edit.table.value;
+    const column = html.edit.column.value;
+
+    if (!title) return;
+
+    const order = state.orders[id];
+    if (!order) return;
+
+    order.title = title;
+    order.table = table;
+    order.column = column;
+
+    const htmlOrder = document.querySelector(`[data-id="${id}"]`);
+    if (column !== order.column) {
+        moveToColumn(id, column);
+    } else {
+        html.area[column].replaceChild(createOrderHtml(order), htmlOrder);
+    }
+
+    html.edit.overlay.classList.remove('edit-overlay--visible');
+};
+
+const handleEditDelete = (event) => {
+    event.preventDefault();
+    const id = html.edit.id.value.trim();
+    if (!id) return;
+    const order = state.orders[id];
+    if (!order) return;
+
+    delete state.orders[id];
+
+    const htmlOrder = document.querySelector(`[data-id="${id}"]`);
+    htmlOrder.remove();
+
+    html.edit.overlay.classList.remove('edit-overlay--visible');
+};
+
+html.other.grid.addEventListener('dragover', handleDragOver);
+html.other.grid.addEventListener('dragstart', handleDragStart);
+html.other.grid.addEventListener('dragend', handleDragEnd);
+
+DragEnd);
+
+function handleDragOver(event) {
+event.preventDefault();
 }
 
-
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
-const handleEditSubmit = (event) => {}
-const handleDelete = (event) => {}
-
-html.add.cancel.addEventListener('click', handleAddToggle)
-html.other.add.addEventListener('click', handleAddToggle)
-html.add.form.addEventListener('submit', handleAddSubmit)
-
-html.other.grid.addEventListener('click', handleEditToggle)
-html.edit.cancel.addEventListener('click', handleEditToggle)
-html.edit.form.addEventListener('submit', handleEditSubmit)
-html.edit.delete.addEventListener('click', handleDelete)
-
-html.help.cancel.addEventListener('click', handleHelpToggle)
-html.other.help.addEventListener('click', handleHelpToggle)
-
-for (const htmlColumn of Object.values(html.columns)) {
-    htmlColumn.addEventListener('dragstart', handleDragStart)
-    htmlColumn.addEventListener('dragend', handleDragEnd)
+function handleDragStart(event) {
+event.dataTransfer.setData("text/plain", event.target.id);
+event.currentTarget.style.backgroundColor = "lightblue";
 }
 
-for (const htmlArea of Object.values(html.area)) {
-    htmlArea.addEventListener('dragover', handleDragOver)
+function handleDragEnd(event) {
+event.currentTarget.style.backgroundColor = "";
+}
+
+function handleDrop(event) {
+event.preventDefault();
+const id = event.dataTransfer.getData("text/plain");
+const draggableElement = document.getElementById(id);
+const dropzone = event.target;
+dropzone.appendChild(draggableElement);
 }
